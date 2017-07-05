b9299c8684eda1fbadaf964cc77b7f826604f60b
likelet <zhaoqi699@sina.com>
2017-07-05 15:32:12 +0800

    modified code to reorder x-axis

--- scripts/Gene cumsum number/Gene cumsum number.R
+++ scripts/Gene cumsum number/Gene cumsum number.R
@@ -2,8 +2,14 @@ rm(list = ls())
 gc()
 require(data.table)
 require(cowplot)
-fuck <- fread('stat_final.txt')
-fuck <- data.table(table(fuck[,1]), cumsum(table(fuck[,1])))
+df <- fread('stat_final.txt')
+fuck <- data.table(table(df[,1]), cumsum(table(df[,1])))
+fuck$V2=as.numeric(fuck$V2)
 setnames(fuck, c('Occurrency', 'gene number', 'cumsum number'))
-fuck <- melt(fuck, id = 1, variable.name = "Type")
-ggplot() + geom_bar(data = fuck, aes(x = Occurrency, y = value, fill = Type), position = 'identity', stat = 'identity', alpha = 0.3) + scale_y_log10(expand = c(0.01, 0)) + scale_fill_manual(values = c('black', '#666666')) + labs(y = 'Gene number')
+fuck <- data.frame(melt(fuck, id = 1, variable.name = "Type"))
+fuck$Occurrency=as.factor(fuck$Occurrency,levels=names(table(df$V1)))
+ggplot() +
+  geom_bar(data = fuck, aes(x = Occurrency, y = value, fill = Type), position = 'identity', stat = 'identity', alpha = 0.3) + 
+  scale_y_log10(expand = c(0.01, 0)) +
+  scale_fill_manual(values = c('black', '#666666')) + 
+  labs(y = 'Gene number')
