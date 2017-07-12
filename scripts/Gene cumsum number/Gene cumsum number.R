5e944ea5900e5407dc23d57845267a7b7cba423a
likelet <zhaoqi699@sina.com>
2017-07-12 13:15:54 +0800

    update mutation dataset

--- scripts/Gene cumsum number/Gene cumsum number.R
+++ scripts/Gene cumsum number/Gene cumsum number.R
@@ -7,7 +7,7 @@ fuck <- data.table(table(df[,1]), cumsum(table(df[,1])))
 fuck$V2=as.numeric(fuck$V2)
 setnames(fuck, c('Occurrency', 'gene number', 'cumsum number'))
 fuck <- data.frame(melt(fuck, id = 1, variable.name = "Type"))
-fuck$Occurrency=as.factor(fuck$Occurrency,levels=names(table(df$V1)))
+fuck$Occurrency=factor(fuck$Occurrency,levels=names(table(df$V1)))
 ggplot() +
   geom_bar(data = fuck, aes(x = Occurrency, y = value, fill = Type), position = 'identity', stat = 'identity', alpha = 0.3) + 
   scale_y_log10(expand = c(0.01, 0)) +
